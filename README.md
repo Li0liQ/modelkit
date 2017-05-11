b6c9ef66696447100a0177324cc5e7f3770e5d13
Li0liQ <li0liq@gmail.com>
2017-05-12 00:43:17 +1000

    Test config linting and badges in readme.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# modelkit
+modelkit
+=========================
+Extensible flagged build config generator.  
+
+[![Build Status](https://travis-ci.org/Li0liQ/modelkit.svg?branch=master)](https://travis-ci.org/Li0liQ/modelkit)
+[![npm version](https://img.shields.io/npm/v/modelkit.svg?style=flat-square)](https://www.npmjs.com/package/modelkit)
--- modelkit.config.js
+++ modelkit.config.js
@@ -3,6 +3,7 @@ const ModelkitJsonLoader = require('./lib/modelkit').ModelkitJsonLoader;
 const ModelkitJsLoader = require('./lib/modelkit').ModelkitJsLoader;
 const ModelkitYarnLoader = require('./lib/modelkit').ModelkitYarnLoader;
 const ModelkitFreezePlugin = require('./lib/modelkit').ModelkitFreezePlugin;
+const ModelkitManifestPlugin = require('./lib/modelkit').ModelkitManifestPlugin;
 
 module.exports = {
     inputDir: path.join(__dirname, 'input'),
@@ -12,64 +13,64 @@ module.exports = {
             changes: [{
                 flag: 'json-1',
                 delete: {
-                    'license': null
+                    license: null,
                 },
                 update: {
-                    'name': 'json-1-generated name'
-                }
+                    name: 'json-1-generated name',
+                },
             }, {
                 flag: 'json-2',
                 delete: {
-                    'dependencies': null
+                    dependencies: null,
                 },
                 update: {
                     'json-2': 'You can create fields on the fly',
                     'json-2.sub': {
-                        'test': 'Even subfields'
-                    }
-                }
+                        test: 'Even subfields',
+                    },
+                },
             }, {
                 flag: 'common-2',
                 update: {
-                    'license': 'A creative commons one'
-                }
+                    license: 'A creative commons one',
+                },
             }],
         }),
         new ModelkitJsLoader({
             files: ['webpack.config.js'],
-            flagFunction: 'getFlag'
+            flagFunction: 'getFlag',
         }),
         new ModelkitYarnLoader({
             files: ['yarn.lock'],
             changes: [{
                 flag: 'yarn-1',
                 delete: {
-                    'splain@^0.0.1': null
+                    'splain@^0.0.1': null,
                 },
                 update: {
                     'lodash@1.3.x': {
                         version: '4.0.0',
-                        resolved: 'http://not.existing.address'
-                    }
-                }
+                        resolved: 'http://not.existing.address',
+                    },
+                },
             }, {
                 flag: 'yarn-2',
                 update: {
-                    'splain@^0.0.1':  {
+                    'splain@^0.0.1': {
                         version: '0.0.2',
                         resolved: 'http://splain.002',
                         dependencies: {
-                            lodash: '1.3.x'
-                        }
+                            lodash: '1.3.x',
+                        },
                     },
                     'splain@^0.0.2': {
                         version: '0.0.2',
                         resolved: 'http://splain.002',
                         dependencies: {
-                            lodash: '1.3.x'
-                        }
-                    }
-                }
+                            lodash: '1.3.x',
+                        },
+                    },
+                },
             }, {
                 flag: 'common-1',
             }],
@@ -83,8 +84,11 @@ module.exports = {
                 'js-1': true,
                 'json-1': true,
                 'common-1': true,
-                'yarn-1': true
-            }
+                'yarn-1': true,
+            },
         }),
-    ]
+        new ModelkitManifestPlugin({
+            file: 'manifest.json',
+        }),
+    ],
 };
